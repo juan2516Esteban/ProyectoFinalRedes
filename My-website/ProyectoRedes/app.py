4a6e0a090b3ce9628c60a4fb7ef87589aae5d31e
Juan Esteban Salazar Londoño <juanesteban.salazar1@utp.edu.co>
2024-05-30 22:54:44 -0500

    Implementación de frontend y actualización de variables y trasformación a estilo JSON

--- My-website/ProyectoRedes/app.py
+++ My-website/ProyectoRedes/app.py
@@ -4,34 +4,34 @@ from Backend import speedTest
 
 app = Flask(__name__)
 functions = speedTest
+Json = []
 
-ip = functions.get_public_ip_address()
 
-@app.route("/" , methods = ['GET'])
+@app.route("/", methods=['GET', 'POST'])
 def speedtest():
-    ip = functions.get_public_ip_address()
-    nombre = functions.get_windows_ssid()
-    IpDevices = functions.get_ip_address()
-    PriavteIp = functions.obtener_ip_privada()
-    return render_template("index.html", ip=ip , nombreDeRed = nombre , IpDevices = IpDevices , PriavteIp = PriavteIp) 
-
-@app.route("/" , methods = ['POST'] )
-def AnchodeBanda():
-    unidades = str(request.form['options'])
-    bytes_sent_gb , bytes_recv_gb = functions.medir_ancho_de_banda_red_local(unidades)
-    if unidades == 'bytes':
-        output = f"Bytes enviados: {bytes_sent_gb} bytes, Bytes recibidos: {bytes_recv_gb} bytes"
-    elif unidades == 'gigabytes':
-        output = f"Bytes enviados: {bytes_sent_gb} GB, Bytes recibidos: {bytes_recv_gb} GB"
+    salida = "hola"
+    if request.method == 'POST':
+        unidades = str(request.form['options'])
+        bytes_sent_gb, bytes_recv_gb = functions.medir_ancho_de_banda_red_local(unidades)
+        if unidades == 'bytes':
+            output = f"Bytes enviados: {bytes_sent_gb} bytes, Bytes recibidos: {bytes_recv_gb} bytes"
+        elif unidades == 'gigabytes':
+            output = f"Bytes enviados: {bytes_sent_gb} GB, Bytes recibidos: {bytes_recv_gb} GB"
+        else:
+            output = 'Unidad no válida'
     else:
-        output = 'Unidad no valida'
+        output = None
 
-    ip = functions.get_public_ip_address()
-    nombre = functions.get_windows_ssid()
-    IpDevices = functions.get_ip_address()
-    PriavteIp = functions.obtener_ip_privada()
-    return render_template("index.html", ip=ip , nombreDeRed = nombre , IpDevices = IpDevices , PriavteIp = PriavteIp, output=output) 
-
+    datos = {
+        "ip": functions.get_public_ip_address(),
+        "nombre": functions.get_windows_ssid(),
+        "IpDevices": functions.get_ip_address(),
+        "PrivateIp": functions.obtener_ip_privada(),
+    }
+    
+    Json = datos
+    salida = output
+    return render_template("redes.html", datos=Json, output=salida)
 
 if __name__ == "__main__":
     app.run(debug=True)
